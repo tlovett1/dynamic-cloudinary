a38c69c483184a1cce2c9957644708786abfac95
Taylor Lovett <tlovett88@gmail.com>
2022-08-15 09:41:13 -0400

    Version bump

--- dynamic-cloudinary.php
+++ dynamic-cloudinary.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Dynamic Cloudinary
  * Plugin URI:        https://github.com/tlovett1/dynamic-cloudinary
  * Description:       Automatically serve all your images optimized from the cloud.
- * Version:           1.0.3
+ * Version:           1.0.4
  * Requires PHP:      7.0
  * Author:            10up
  * Author URI:        https://10up.com
@@ -17,7 +17,7 @@
 
 namespace DynamicCloudinary;
 
-define( 'DYNAMIC_CLOUDINARY_VERSION', '1.0.3' );
+define( 'DYNAMIC_CLOUDINARY_VERSION', '1.0.4' );
 define( 'DYNAMIC_CLOUDINARY_URL', plugin_dir_url( __FILE__ ) );
 define( 'DYNAMIC_CLOUDINARY_PATH', plugin_dir_path( __FILE__ ) );
 
